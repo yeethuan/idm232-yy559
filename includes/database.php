fe6ede56e57a4abc12e0f9dd0e935cb5d11d852f
Yi <yy559@drexel.edu>
2024-12-06 16:19:36 -0500

    pictures and database files and htaccess files

---
+++ includes/database.php
@@ -0,0 +1,12 @@
+$db_server = getenv('DB_SERVER');
+$db_username = getenv('DB_USERNAME');
+$db_password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully. MySQL version: " . mysqli_get_server_info($connection);
